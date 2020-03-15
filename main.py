4a7ba85d717009030115ef476082466614b38db7
Puneet Saini <puneet29saini@gmail.com>
2020-03-15 23:55:12 +0530

    Structured the code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import psycopg2
 from config import config
 
+
 def connect():
     """ Connect to the PostgreSQL database server"""
     conn = None
@@ -11,7 +12,23 @@ def connect():
         # Connect to the PostgreSQL server
         print("Connecting to the PostgreSQL database...")
         conn = psycopg2.connect(**params)
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            return conn
 
+def disconnect(conn):
+    """ Disconnect PostgreSQL database server """
+    if conn is None:
+        return
+
+    conn.close()
+    print("Database connection closed")
+
+def print_version(conn):
+    """ Print the version of PostgreSQL running """
+    try:
         # Create a cursor
         cur = conn.cursor()
 
@@ -27,12 +44,9 @@ def connect():
         cur.close()
     except(Exception, psycopg2.DatabaseError) as error:
         print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print("Database connection closed.")
-
-
+    
 if __name__ == "__main__":
-    connect()
+    conn = connect()
+    print_version(conn)
+    disconnect(conn)
 
